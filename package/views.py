9ad7cc4fbca93bc7932a66e0f718132d94a5b639
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-07 15:28:41 +0330

    create PricingView

--- package/views.py
+++ package/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from package.models import Package
 
-# Create your views here.
+
+class PricingView(View):
+    def get_context_data(self):
+        packages = Package.objects.filter(is_enable=True)
+        return dict(packages=packages)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "package/pricing.html", self.get_context_data())
