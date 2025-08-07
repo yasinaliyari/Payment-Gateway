a9554acff4b17f312100164ab42c899181028399
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-07 18:52:15 +0330

    register Purchase

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from purchase.models import Purchase
 
-# Register your models here.
+
+@register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ["user", "package", "price", "status"]
+    list_filter = ["status"]
