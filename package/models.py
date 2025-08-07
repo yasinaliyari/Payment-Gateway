001e5c4c26513f478507a38a510abb95135b9033
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-07 15:05:57 +0330

    create Package model

--- package/models.py
+++ package/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    title = models.CharField(max_length=48)
+    price = models.PositiveBigIntegerField()
+    description = models.TextField(blank=True)
+    days = models.PositiveBigIntegerField()
+    is_enable = models.BooleanField(default=True)
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
