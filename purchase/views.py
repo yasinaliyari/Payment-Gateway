9ecc0505a30be24c11d7f399c0cb2b66ab78f8be
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-07 18:51:38 +0330

    create PurchaseCreateView

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.shortcuts import render
+from django.views import View
+from package.models import Package
+from purchase.models import Purchase
 
-# Create your views here.
+
+class PurchaseCreateView(LoginRequiredMixin, View):
+    def get(self, request, package_id, *args, **kwargs):
+        try:
+            package = Package.objects.get(id=package_id)
+        except Package.DoesNotExist:
+            raise Http404
+
+        purchase = Purchase.create(package, request.user)
+        return render(request, "purchase/create.html", {"purchase": purchase})
