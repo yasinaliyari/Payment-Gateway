2a859dfc5e18e06e05e8e63976af86c913b0041f
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-08 23:55:46 +0330

    add pay url

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from finance.views import ChargeWalletView, VerifyView
+from finance.views import ChargeWalletView, VerifyView, PaymentView, PaymentGatewayView
 
 urlpatterns = [
     path("charge/", ChargeWalletView.as_view(), name="charge-wallet"),
     path("verify/", VerifyView.as_view(), name="verify-view"),
+    path("pay/<str:invoice_number>/", PaymentView.as_view(), name="payment-link"),
+    path(
+        "pay/<str:invoice_number>/<str:gateway_code>/",
+        PaymentGatewayView.as_view(),
+        name="payment-gateway",
+    ),
 ]
