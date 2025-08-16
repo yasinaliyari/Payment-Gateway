1948209a7a469bbedeb52ab1f8943a75b092edf1
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-17 00:23:25 +0330

    create PurchaseListView class with method_decorator

--- purchase/views.py
+++ purchase/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from package.models import Package
 from purchase.models import Purchase
@@ -26,3 +28,13 @@ def purchases_list(request, username):
         purchases = purchases.filter(user__username=username)
     print("View touched")
     return render(request, "purchase/list.html", {"purchases": purchases})
+
+
+@method_decorator([cache_page(200), vary_on_cookie], name="dispatch")
+class PurchaseListView(View):
+    def get(self, request, username=None, *args, **kwargs):
+        purchases = Purchase.objects.all()
+        if username is not None:
+            purchases = purchases.filter(user__username=username)
+        print("View touched")
+        return render(request, "purchase/list.html", {"purchases": purchases})
