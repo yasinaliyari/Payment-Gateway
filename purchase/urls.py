fff73414b7cef0e3e68bc37fd6776c1b0d6575ae
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-16 23:58:52 +0330

    add list/ address

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
-from purchase.views import PurchaseCreateView
+from purchase.views import PurchaseCreateView, PurchaseListView
 
 urlpatterns = [
     path(
         "create/<int:package_id>/", PurchaseCreateView.as_view(), name="create-purchase"
-    )
+    ),
+    path("list/", PurchaseListView.as_view(), name="list-purchase"),
 ]
