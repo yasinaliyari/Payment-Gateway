39ec6bf6c79eae8af23243e984af51f872605223
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-17 16:13:10 +0330

    add gettext_lazy and translate

--- package/models.py
+++ package/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Package(models.Model):
-    title = models.CharField(max_length=48)
-    price = models.PositiveBigIntegerField()
+    title = models.CharField(max_length=48, verbose_name=_("title"))
+    price = models.PositiveBigIntegerField(verbose_name=_("price"))
     description = models.TextField(blank=True)
     days = models.PositiveBigIntegerField()
     is_enable = models.BooleanField(default=True)
 
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Package")
+        verbose_name_plural = _("Packages")
 
     def __str__(self):
         return self.title
