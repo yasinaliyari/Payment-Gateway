04c5a875c1af2e22619fb69d1cf8a506aacf0c38
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-18 22:56:11 +0330

    create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name=_("title"), max_length=255)
+    body = models.TextField(verbose_name=_("body"))
+    create_time = models.DateTimeField(verbose_name=_("create time"), auto_now_add=True)
+    modified_time = models.DateTimeField(verbose_name=_("modified time"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+
+    def __str__(self):
+        return self.title
