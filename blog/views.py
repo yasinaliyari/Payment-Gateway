b4606dc758892eb7c087703b19925c5d34af2753
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-18 22:57:39 +0330

    create Post view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
 
-# Create your views here.
+
+class BlogPostListView(ListView):
+    model = Post
+    context_object_name = "posts"
